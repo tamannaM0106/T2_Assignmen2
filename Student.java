5d307d39639fd9302fe442927e0664e023140d52
Tamanna <o1fe22bca129@kletech.ac.in>
2024-02-07 13:27:38 +0530

    Code for Displaying Name & DOB of Student

--- Student.java
+++ Student.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 /**
  * The Student class represents a student with a name and date of birth.
  */
-public class Student{
+public class Student extends StudentCourse{
     public String name;
     public String dob;
 
@@ -36,8 +36,11 @@ public class Student{
      */
     public static void main(String[] args) {
         Student s=new Student();
+        StudentCourse s1= new StudentCourse();
         s.name = "Tamanna D Mahajan";
         s.displayName();
         s.displayAge("01-06-2004");
+        s1.displayStudentcourses();
+        s1.displayStudentMarks();
        }
 }
