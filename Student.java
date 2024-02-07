607564e9d0dcd0d7895886af119ea383b309b7e8
Tamanna <o1fe22bca129@kletech.ac.in>
2024-02-07 13:23:29 +0530

    Code for Displaying Name & DOB of Student

---
+++ Student.java
@@ -0,0 +1,43 @@
+// The `import` statements in Java are used to bring in external classes or packages into your code.
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * The Student class represents a student with a name and date of birth.
+ */
+public class Student{
+    public String name;
+    public String dob;
+
+    /**
+     * The function "displayName" prints the name of a student.
+     */
+    public void displayName() {
+        System.out.println("Student Name: " + name);
+    }
+
+    // The `displayAge` method in the `Student` class is used to calculate and display the age of a
+    // student based on their date of birth (`dob`).
+    public void displayAge(String dob) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate birthDate = LocalDate.parse(dob, formatter);
+        LocalDate currentDate = LocalDate.now();
+        Period period = Period.between(birthDate, currentDate);
+        int age = period.getYears();
+        System.out.println("Age of student: " + age +"years");
+
+
+    }
+
+    /**
+     * The main function creates a new Student object, sets the name attribute, and then calls the
+     * displayName and displayAge methods.
+     */
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.name = "Tamanna D Mahajan";
+        s.displayName();
+        s.displayAge("01-06-2004");
+       }
+}
